c1e6f4ce72b6297078ba035f181581a64a93af9e
Leandro Toniut <leandro.toniut@levvel.io>
2020-08-18 10:21:14 -0300

    Changed ReminderDetails form to Modal

--- src/components/ReminderDetails.tsx
+++ src/components/ReminderDetails.tsx
@@ -1,4 +1,11 @@
-import { AutoComplete, Button, DatePicker, Input, TimePicker } from "antd";
+import {
+  AutoComplete,
+  Button,
+  DatePicker,
+  Input,
+  TimePicker,
+  Modal,
+} from "antd";
 import { css, cx } from "emotion";
 import * as AP from "fp-ts/lib/Apply";
 import * as A from "fp-ts/lib/Array";
@@ -38,7 +45,7 @@ export const Reminder = (props: Props) => {
   const [city, setCity] = useState(props.city);
   const [color, setColor] = useState<string>(props.color);
   const [message, setMessage] = useState<string>(props.message);
-  const [weatherO, setWeatherO] = useState<O.Option<WeatherC>>(O.none);
+  const [weatherO, setWeatherO] = useState<O.Option<string>>(O.none);
 
   const updatedData: () => Props = () => ({
     id: props.id,
@@ -94,7 +101,18 @@ export const Reminder = (props: Props) => {
                   time,
                 })()
               ).subscribe((x) =>
-                pipe(x, E.fold(constVoid, flow(O.some, setWeatherO)))
+                pipe(
+                  x,
+                  E.fold(
+                    constVoid,
+                    flow(
+                      (x) => x.weather,
+                      A.head,
+                      O.map((x) => x.description),
+                      setWeatherO
+                    )
+                  )
+                )
               )
             )
         )
@@ -103,122 +121,79 @@ export const Reminder = (props: Props) => {
   );
 
   return (
-    <form
-      className={styles.component({
-        x: props.positionX,
-        y: props.positionY,
-        goesLeft: props.goesLeft,
-      })}
-      action=""
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
+    <Modal
+      title={message || "New Reminder"}
+      visible={true}
+      onOk={() => props.saveReminder(updatedData(), props.id)}
+      onCancel={() => props.deleteReminder(props.id)}
     >
-      <div>
-        <DatePicker
-          className={styles.datePicker}
-          defaultValue={moment(props.day.toRFC2822(), "DD-MMM-YYYY")}
-          onChange={(e) => {
-            if (e) setDay(DateTime.fromJSDate(e.toDate()));
-          }}
-        />
-      </div>
-      <div>
-        <TimePicker
-          className={styles.timePicker}
-          value={O.toNullable(timeO)}
-          onChange={flow(O.fromNullable, setTimeO)}
-        />
-      </div>
-      <div>
-        <Input
-          className={styles.message}
-          name="message"
-          placeholder="Write some message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </div>
-      <div>
-        <AutoComplete
-          className={styles.searchCity}
-          placeholder="Search city"
-          onChange={(e) => cityInput$.next(e)}
-          onSelect={(e) => setCity(e)}
-          options={A.array.map(cityOptions, (x) => ({ label: x, value: x }))}
-        />
-      </div>
-      <div>
-        {pipe(
-          weatherO,
-          O.fold(
-            () => <div>The weather is...</div>,
-            ({ weather }) => (
-              <div>
-                <div>Weather</div>
-                <div>
-                  {JSON.stringify(
-                    weather[0].description.charAt(0).toUpperCase() +
-                      weather[0].description.slice(1)
-                  )}
-                </div>
-              </div>
+      <div className={styles.container}>
+        <div>
+          <DatePicker
+            className={styles.datePicker}
+            defaultValue={moment(props.day.toRFC2822(), "DD-MMM-YYYY")}
+            onChange={(e) => {
+              if (e) setDay(DateTime.fromJSDate(e.toDate()));
+            }}
+          />
+        </div>
+        <div>
+          <TimePicker
+            className={styles.timePicker}
+            value={O.toNullable(timeO)}
+            onChange={flow(O.fromNullable, setTimeO)}
+          />
+        </div>
+        <div>
+          <Input
+            className={styles.message}
+            name="message"
+            placeholder="Write some message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <div>
+          <AutoComplete
+            className={styles.searchCity}
+            placeholder="Search city"
+            onChange={(e) => cityInput$.next(e)}
+            onSelect={(e) => setCity(e)}
+            options={A.array.map(cityOptions, (x) => ({ label: x, value: x }))}
+          />
+        </div>
+        <div>
+          {pipe(
+            weatherO,
+            O.fold(
+              () => <div>The weather is...</div>,
+              (weather) => <div>{`The weather is ${weather}`}</div>
             )
-          )
-        )}
+          )}
+        </div>
+        <div className={styles.colorSelector}>
+          {pipe(
+            colorChoices,
+            A.map((r) => (
+              <div
+                className={cx([
+                  styles.color(r),
+                  ...(color === r ? [styles.selectedColor] : []),
+                ])}
+                onClick={() => setColor(r)}
+              />
+            ))
+          )}
+        </div>
       </div>
-      <div className={styles.colorSelector}>
-        {pipe(
-          colorChoices,
-          A.map((r) => (
-            <div
-              className={cx([
-                styles.color(r),
-                ...(color === r ? [styles.selectedColor] : []),
-              ])}
-              onClick={() => setColor(r)}
-            />
-          ))
-        )}
-      </div>
-      <Button
-        disabled={pipe(
-          { timeO },
-          AP.sequenceS(O.option),
-          O.isSome,
-          not(identity)
-        )}
-        onClick={() => props.saveReminder(updatedData(), props.id)}
-      >
-        Save
-      </Button>
-      <Button onClick={() => props.deleteReminder(props.id)}>Delete</Button>
-    </form>
+    </Modal>
   );
 };
 
 const styles = {
-  component: ({
-    x,
-    y,
-    goesLeft,
-  }: {
-    x: number;
-    y: number;
-    goesLeft: boolean;
-  }) => css`
-    background-color: white;
-    border: 1px solid grey;
-    display: flex;
-    flex-direction: column;
-    color: #000000;
-    position: absolute;
-    text-align: center;
-    width: 200px;
-    padding: 0.5rem;
-    ${direction(x, y, goesLeft)};
+  container: css`
     > * {
-      margin-bottom: 0.5rem;
+      margin-bottom: 1rem;
     }
   `,
   datePicker: css`
@@ -239,11 +214,11 @@ const styles = {
   `,
   colorSelector: css`
     display: grid;
-    grid-gap: 0.25rem;
+    grid-gap: 0.5rem;
     grid-template-columns: repeat(auto-fit, 2rem);
   `,
   selectedColor: css`
-    border: 2px black solid;
+    border: 3px #4fc3f7 solid;
   `,
   color: (color: string) => css`
     background-color: ${color};
@@ -251,6 +226,7 @@ const styles = {
     height: 2rem;
     float: left;
     border-style: solid;
+    border-color: #4fc3f7;
     border-width: thin;
   `,
 };
